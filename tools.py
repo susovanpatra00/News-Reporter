fa8694fa51d8bafd77214bccde44b65035846fe7
Susovan Patra <susovanpatra00@gmail.com>
2024-07-13 12:33:29 +0530

    Add files via upload

---
+++ tools.py
@@ -0,0 +1,13 @@
+## https://serper.dev/
+
+from crewai_tools import SerperDevTool
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+
+os.environ['SERPER_API_KEY'] = os.getenv('SERPER_API_KEY')
+
+
+# Initialize the tool for internet searching capabilities
+tool = SerperDevTool()
